7146a722d475bde3b51a6663234f58b663e35873
Rahul Sahay <rahulsahay19@hotmail.com>
2017-10-04 21:48:30 +0530

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 
-
+const appRoutes:Routes=[
+ {path:'',component:HomeComponent},
+ {path:'reviews',component:ReviewsComponent},
+ {path:'movies',component:MoviesComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +19,8 @@ import { ReviewsComponent } from './reviews/reviews.component';
     ReviewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
