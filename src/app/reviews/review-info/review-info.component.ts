d4a435ea1303a0e9e793454e21d168c2f920cf28
Rahul Sahay <rahulsahay19@hotmail.com>
2017-10-17 13:10:02 +0530

    Created Child Route Files

---
+++ src/app/reviews/review-info/review-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-review-info',
+  templateUrl: './review-info.component.html',
+  styleUrls: ['./review-info.component.css']
+})
+export class ReviewInfoComponent {
+  
+  constructor() { }
+  
+
+}
---
+++ src/app/reviews/reviewers-info/reviewers-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-reviewers-info',
+  templateUrl: './reviewers-info.component.html',
+  styleUrls: ['./reviewers-info.component.css']
+})
+export class ReviewersInfoComponent {
+  
+  constructor() { }
+  
+
+}
